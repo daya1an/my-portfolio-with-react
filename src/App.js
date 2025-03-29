033a77603c8b298c1fd5424878027f7216b35a52
Dayalan D S <dayalands2210@gmail.com>
2025-03-29 12:13:13 +0530

    gh deploy changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {BrowserRouter} from "react-router-dom";
 function App() {
    return (
       <div>
-         <BrowserRouter>
+         <BrowserRouter basename="/my-portfolio-with-react">
             <BaseLayout/>
          </BrowserRouter>
       </div>
